2e71d238724637f3dd9938ad1250631532480e22
Alisson Neves <neves_alisson@hotmail.com>
2025-02-15 00:43:49 -0300

    Debugging bankaccounts redirect http error

--- BankAccounts/Program.cs
+++ BankAccounts/Program.cs
@@ -7,14 +7,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ConfigureHttpsDefaults(config =>
+    {
+        config.SslProtocols = System.Security.Authentication.SslProtocols.Tls12;
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
